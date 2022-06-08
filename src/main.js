388c4f154c02c0d50aec747ebf67b95e7509c461
Evgeniy <95763654+YouloveSoulmate@users.noreply.github.com>
2022-06-08 12:19:41 +0300

    подключен vuex

--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@ import { createApp } from 'vue';
 import App from '@/App.vue'
 
 import router from '@/routes'
+import store from '@/store'
 
 import '@/assets/scss/main.scss'
 import '@/assets/scss/common.scss'
 
 const app = createApp(App);
 app.use(router);
+app.use(store);
 
 // app.use(ThePluginIWantToUse)
 
